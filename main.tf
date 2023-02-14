b6174875be023fb033fe8e45c88ed9087c6e70aa
CrimsonGlacier <120449326+CrimsonGlacier@users.noreply.github.com>
2023-02-13 20:37:24 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,6 +7,13 @@ terraform {
     }
   }
 }
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-github-actions-state"
+    storage_account_name = "terraformgithubactions"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+    use_oidc             = true
+  }
 provider "azurerm" {
   features {
     resource_group {
